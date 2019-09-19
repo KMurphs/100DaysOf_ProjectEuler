b80ebfcd7590dcf8f50ec7b33848f27eda880bc4
KMurphs <KibongeSP@gmail.com>
2019-09-19 16:36:26 +0200

    Problem 9: A theoretical number approach using primitive pythagorian triplet generation yields faster results

--- python/ProjectEuler_9.py
+++ python/ProjectEuler_9.py
@@ -28,7 +28,7 @@ def get_pythagorian_triplets_with_sum(N):
 					# print(f"Found Pythagorian triplets: {a,b,c} with sum {a+b+c} (c square is {c*c})")
 					if(a+b+c==N):
 						print(f"	Found Solution for triplets: {a,b,c} with sum {a+b+c} (c square is {c*c})")
-						return a,b,c,a*b*c
+						return a,b,c,a*b*c,a+b+c,a*a+b*b,c*c
 
 
 	return "[No Data]"
@@ -48,8 +48,7 @@ def get_pythagorian_triplets_with_sum_v2(N):
 			a = N - b - c
 			if((a*a)+(b*b)==(c*c)):
 				print(f"	Found Solution for triplets: {a,b,c} with sum {a+b+c} (c square is {c*c})")
-				return a,b,c,a*b*c
-
+				return a,b,c,a*b*c,a+b+c,a*a+b*b,c*c
 
 	return "[No Data]"
 
@@ -61,10 +60,12 @@ def get_pythagorian_triplets_with_sum_v3(N):
 	c = 1
 	# using N=a+b+c, and c^2=a^2+b^2, with tan(alpha) = b/a, cos(alpha) = a/c, tan(alpha/2) = t
 	# t = 1 - 2a/N, b = a*2t/(1-t^2), c = a (1+t^2)/(1-t^2)
-	a = int(N/3+1) + 1
+	# a = int(N/3+1) + 1
+	a = 1
 	for _ in range(int(N/3+1)):
 		
-		a -= 1
+		# a -= 1
+		a += 1
 
 		# t = 1 - 2*a/N
 		# t_sqr = (1 - 2*a/N)*(1 - 2*a/N)
@@ -76,16 +77,82 @@ def get_pythagorian_triplets_with_sum_v3(N):
 		b = (N*N-2*a*N)/(2*N-2*a)
 		if(a>b): continue
 
-		# c = ((N*N)-(2*a*N)+(2*a*a))/(2*N-2*a)
-		c = math.pow(a*a+b*b, 0.5)
+		c = ((N*N)-(2*a*N)+(2*a*a))/(2*N-2*a)
+		# c = math.pow(a*a+b*b, 0.5)
 		if(b>c): continue
 
 		# if((int(a)+int(b)+int(c)==int(N)) & (a<b) & (b<c)):
-		# if((a+int(b)+int(c)==N)):
-		if(int(a+b+c)==N):
+		if((a+int(b)+int(c)==N)):
+		# if(int(a+b+c)==N):
 			# print(f"	Found Solution for triplets: {a,b,c} with sum {a+b+c} (c square is {c*c})")
-			return a,b,c,a*b*c
+			return a,b,c,a*b*c,a+b+c,a*a+b*b,c*c
 		
+
+	return "[No Data]"
+
+
+
+
+
+
+def gcd(a, b):
+	y = 0;
+	x = 0;
+ 
+	if (a > b):
+		x = a
+		y = b
+	else:
+		x = b
+		y = a
+	
+	while (x % y != 0): 
+		temp = x
+		x = y
+		y = temp % x
+
+	return y
+
+
+# https://www.mathblog.dk/pythagorean-triplets/
+def get_pythagorian_triplets_with_sum_by_number_theory(N):
+
+	a=0
+	b=0
+	c=0
+	s = N
+
+	m = 0
+	k = 0
+	n = 0
+	d = 0
+
+	found = False
+
+	mlimit = int(math.sqrt(s / 2))
+	for m in range(2, mlimit + 1):
+		if ((s // 2) % m == 0): # m found
+			if (m % 2 == 0): #  ensure that we find an odd number for k
+				k = m + 1
+			else:
+				k = m + 2;
+
+			while ((k < (2 * m)) & (k <= (s / (2 * m)))):
+				if (((s / (2 * m)) % k == 0) & (gcd(k, m) == 1)):
+					d = s / (2 * (k * m))
+					n = k - m
+					a = d*(m * m - n * n)
+					b = 2 * d * n * m
+					c = d * (m * m + n * n)
+					found = True
+					break
+
+				k += 2
+
+		if found:
+			return a,b,c,a*b*c,a+b+c,a*a+b*b,c*c
+			# break;
+			
 
 	return "[No Data]"
 
@@ -108,6 +175,11 @@ if __name__ == "__main__":
 
 	start = time.time()
 	print(get_pythagorian_triplets_with_sum_v3(N))
+	end = time.time()
+	print(f"Operation took {1000*(end - start):.2f}ms")
+
+	start = time.time()
+	print(get_pythagorian_triplets_with_sum_by_number_theory(N))
 	end = time.time()
 	print(f"Operation took {1000*(end - start):.2f}ms")
 
